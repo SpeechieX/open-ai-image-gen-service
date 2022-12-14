1e39354bc46973ae4a2df23e4fe7d8487e854acb
Erik HR <writeerikh@gmail.com>
2022-12-14 14:38:52 +0700

    connected to frontend, cors added

--- controller/openaiController.js
+++ controller/openaiController.js
@@ -7,12 +7,13 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 const generateImage = async (req, res) => {
-	const { prompt } = req.body;
+	let prompt = req.body.prompt;
+
 	try {
 		const response = await openai.createImage({
 			prompt: prompt,
 			n: 1,
-			size: "1024x1024",
+			size: "512x512",
 		});
 		const imgUrl = response.data.data[0].url;
 
@@ -22,7 +23,7 @@ const generateImage = async (req, res) => {
 		});
 	} catch (err) {
 		if (err.response) {
-			console.log(err.response.status, error.response.data);
+			console.log(err.response.status, err.response.data);
 		} else {
 			console.log(err.message);
 		}
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
-
-app.use("/openai", require("./routes/openaiRoutes"));
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => {
 	console.log("Open AI Server Running");
